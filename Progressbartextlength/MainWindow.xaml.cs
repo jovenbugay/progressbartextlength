d9fe4f44a868822bf9ede23b75911396aebb9574
jovenbugay <jovenbugay@gmail.com>
2020-09-27 16:07:49 +0800

    update upto 09.27.2020

---
+++ Progressbartextlength/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Progressbartextlength
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        double maxLength = 30;
+        bool isRendered = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            isRendered = true;
+            pbStatus.Minimum = 0;
+            pbStatus.Maximum = 100;
+            Textbox.Text = "";
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!isRendered)
+            {
+                return;
+            }
+            if (Textbox.Text.Length == 0)
+            {
+                pbStatus.IsIndeterminate = true;
+                pbStatus.Value = Textbox.Text.Length;
+            }
+            if (Textbox.Text.Length > 0)
+            {
+                pbStatus.Value = Textbox.Text.Length * 3.333333333333;
+                pbStatus.IsIndeterminate = false;
+            }
+            if (Textbox.Text.Length <= maxLength)
+            {
+                pbStatus.Foreground = Brushes.Green;
+
+            }
+            if (Textbox.Text.Length > maxLength)
+            {
+                pbStatus.Foreground = Brushes.Red;
+            }
+        }
+    }
+}
